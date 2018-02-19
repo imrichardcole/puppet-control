83449bd614b97863d65ed12eec7156e00b22b4ac
Richard Cole <richandkerrycole@gmail.com>
2018-02-19 18:32:29 +0000

    test

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,10 +1,15 @@
-node agent.home {
-  file { '/tmp/example.txt':
-    ensure => present,
-    content => "This will only be present here $node"
-  }
+node agent {
   user { 'git':
     ensure => present,
     managehome => true,
+  }
+}
+
+node master {
+  cron { 'logrotate':
+    command => '/usr/bin/git clone foo.com',
+    user    => 'root',
+    hour    => 2,
+    minute  => 0,
   }
 }
